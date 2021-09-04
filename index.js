a166c0313ded5dc908bd0019b16534cd3dd25e61
Ajmal KV <reallyaju@gmail.com>
2021-09-04 09:13:01 +0530

    added event for click in canvas

--- index.js
+++ index.js
@@ -8,59 +8,68 @@ let lines = Array(240)
     yaxis: Math.round(Math.random() * (50 - 20 + 1) + 20),
     height: Math.round(Math.random() * (125 - 50 + 1) + 50),
   }));
-//   console.log(lines)
 
 let count = 1;
 button.addEventListener("click", () => {
   if (count % 2 === 1) {
     start();
-    // button.innerText = "pause";
-    button.style.backgroundImage = "url(https://image.flaticon.com/icons/png/512/61/61180.png)";
+    button.style.backgroundImage =
+      "url(https://image.flaticon.com/icons/png/512/61/61180.png)";
   } else {
     pause();
-    button.style.backgroundImage = "url(https://www.pinclipart.com/picdir/middle/217-2172073_free-music-icons-play-button-icon-minimalist-clipart.png)";
+    button.style.backgroundImage =
+      "url(https://www.pinclipart.com/picdir/middle/217-2172073_free-music-icons-play-button-icon-minimalist-clipart.png)";
   }
   count++;
 });
 let interval;
 function start() {
-    console.log("afds")
   let line = 0;
   interval = setInterval(() => {
     ctx.fillStyle = "#ff00cc";
     ctx.fillRect(line * 5, lines[line].yaxis, 3, lines[line].height);
     line++;
-}, 500);
+  }, 500);
 }
 
 function pause() {
-    console.log("aaaaa")
-    clearInterval(interval);
+  clearInterval(interval);
 }
 
 function createLines() {
   lines.map((item, i) => {
     if (i === 3 || i === 52 || i === 158 || i === 221) {
-        ctx.fillStyle = "green";
-        ctx.fillRect(i * 5, 0, 3, item.yaxis*2);
-        ctx.fillRect(i * 5 - 15, 0, 52, 15);
-        ctx.fillStyle = "white";
-        if (i === 3) {
-          ctx.fillText("Introduction", 0, 10);
-        } else if (i === 52) {
-          ctx.fillText("Rapport", i * 5 - 15, 10);
-        } else if (i === 158) {
-          ctx.fillText("Maths", i * 5 - 15, 10);
-        } else if (i === 221) {
-          ctx.fillText("Referrals", i * 5 - 15, 10);
-        }
-        ctx.fillStyle = "#63625e";
-        ctx.fillRect(i * 5, item.yaxis, 3, item.height);
+      ctx.fillStyle = "green";
+      ctx.fillRect(i * 5, 0, 3, item.yaxis * 2);
+      ctx.fillRect(i * 5 - 15, 0, 52, 15);
+      ctx.fillStyle = "white";
+      if (i === 3) {
+        ctx.fillText("Introduction", 0, 10);
+      } else if (i === 52) {
+        ctx.fillText("Rapport", i * 5 - 15, 10);
+      } else if (i === 158) {
+        ctx.fillText("Maths", i * 5 - 15, 10);
+      } else if (i === 221) {
+        ctx.fillText("Referrals", i * 5 - 15, 10);
       }
-      else{      
       ctx.fillStyle = "#63625e";
       ctx.fillRect(i * 5, item.yaxis, 3, item.height);
-      }
+    } else {
+      ctx.fillStyle = "#63625e";
+      ctx.fillRect(i * 5, item.yaxis, 3, item.height);
+    }
   });
 }
 createLines();
+canvas.addEventListener("click", (event) => {
+  let xaxis = event.clientX - canvas.offsetLeft;
+  for (let i = 0; i < lines.length; i++) {
+    if (xaxis >= i * 5 && xaxis <= i * 5 + 5) {
+      createLines();
+      ctx.fillStyle = "#ff00cc";
+      for (let j = 0; j <= i; j++) {
+        ctx.fillRect(j * 5, lines[j].yaxis, 3, lines[j].height);
+      }
+    }
+  }
+});
