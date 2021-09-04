d9cc2d9a27147964441d625c8b63790cb458f954
Ajmal KV <reallyaju@gmail.com>
2021-09-04 08:16:46 +0530

    created lines

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const startButton = document.getElementById("start");
 const pauseButton = document.getElementById("pause");
 const ctx = canvas.getContext("2d");
 
-let lines = Array(60)
+let lines = Array(240)
   .fill()
-  .map(() => Math.round(Math.random() * (100 - 20 + 1) + 20));
-console.log(lines);
-
+  .map((u) => ({
+    y: Math.round(Math.random() * (50 - 20 + 1) + 20),
+    h: Math.round(Math.random() * (125 - 50 + 1) + 50),
+  }));
+//   console.log(lines)
 
 startButton.addEventListener("click", () => {});
 pauseButton.addEventListener("click", () => {});
@@ -16,5 +18,10 @@ function start() {}
 
 function pause() {}
 
-function createLines() {}
+function createLines() {
+  lines.map((item, i) => {
+    ctx.fillStyle = "#63625e";
+    ctx.fillRect(i * 5, item.y, 3, item.h);
+  });
+}
 createLines();
