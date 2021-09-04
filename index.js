e9eab71965e9efeed1546105187bfc09035e6b16
Ajmal KV <reallyaju@gmail.com>
2021-09-04 08:13:05 +0530

    added event listeners and functions

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-let lines = Array(60).fill().map(() => Math.round(Math.random() * (100-20 + 1) + 20))
+const canvas = document.getElementById("canvas");
+const startButton = document.getElementById("start");
+const pauseButton = document.getElementById("pause");
+const ctx = canvas.getContext("2d");
+
+let lines = Array(60)
+  .fill()
+  .map(() => Math.round(Math.random() * (100 - 20 + 1) + 20));
 console.log(lines);
+
+
+startButton.addEventListener("click", () => {});
+pauseButton.addEventListener("click", () => {});
+
+function start() {}
+
+function pause() {}
+
+function createLines() {}
+createLines();
